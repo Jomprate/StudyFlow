13d82f7b37b2f33fbb739048db687dd0b30a20b8
SS <srodriguez841@hotmail.com>
2024-09-06 12:54:10 -0500

    Updated Modals, the icon was changed, created RecoverPasswordModal

--- StudyFlow/StudyFlow.Frontend/src/components/index.ts
+++ StudyFlow/StudyFlow.Frontend/src/components/index.ts
@@ -3,8 +3,12 @@ export { default as Brand } from "./brand/Brand";
 export { default as CTA } from "./cta/CTA";
 export { default as Feature } from "./feature/Feature";
 export { default as Navbar } from "./navBar/Navbar";
+
 export { default as AuthModal } from "./modals/authModal/AuthModal";
 export { default as LoginModal } from "./modals/loginModal/LoginModal";
+export { default as RecoverPasswordModal } from "./modals/recoverPasswordModal/RecoverPasswordModal"
+/*export { default as AuthModalManager } from "./modals/authModalManager/AuthModalManager";*/
+
 export { default as Card } from "./card/Card";
 export { default as Slider } from "./slider/Slider";
 export { default as NavbarLoggedIn } from "./navBarLoggedIn/NavbarLoggedIn";
--- StudyFlow/StudyFlow.Frontend/src/components/modals/loginModal/LoginModal.tsx
+++ StudyFlow/StudyFlow.Frontend/src/components/modals/loginModal/LoginModal.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { useTheme } from '../../../ThemeContext';
 import { handleEmailValidation } from '../../../helpers/validationHelpers';
 import { FaLock, FaLockOpen } from 'react-icons/fa';
+import RecoverPasswordModal from '../recoverPasswordModal/RecoverPasswordModal';
 
 interface LoginModalProps {
     open: boolean;
@@ -14,6 +15,7 @@ interface LoginModalProps {
 const LoginModal: React.FC<LoginModalProps> = ({ open, setOpen }) => {
     const { t } = useTranslation();
     const { theme } = useTheme();
+    const [openRecoverPasswordModal, setOpenRecoverPasswordModal] = useState(false);
 
     const { control, handleSubmit, formState: { errors } } = useForm({
         mode: 'onSubmit',
@@ -25,7 +27,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, setOpen }) => {
 
     const [showPassword, setShowPassword] = useState(false);
 
-    const onSubmit = async (data: any) => {
+    const onSubmit = (data: any) => {
         console.log("Form Data:", data);
         setOpen(false);
     };
@@ -40,76 +42,102 @@ const LoginModal: React.FC<LoginModalProps> = ({ open, setOpen }) => {
         }
     };
 
+    const handleRecoverPasswordClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        e.preventDefault();
+        setOpenRecoverPasswordModal(true);
+        setOpen(false);
+    };
+
+    const handleBackToLogin = () => {
+        setOpen(true);
+        setOpenRecoverPasswordModal(false);
+    };
+
     return (
-        <div className={`modal-overlay ${open ? 'show' : ''}`} onClick={handleOverlayClick}>
-            <div className={`login-modal ${theme}`}>
-                {/* Botón de cerrar */}
-                <button className="close-button" onClick={() => setOpen(false)}>
-                    &times;
-                </button>
-                <div className="login-modal-header">{t('global_login')}</div>
-                <div className="login-modal-body">
-                    <form onSubmit={handleSubmit(onSubmit)} className="login-modal-form">
-                        <div className="login-modal-field">
-                            <label className="login-modal-label">{t('global_Email')}</label>
-                            <Controller
-                                name="email"
-                                control={control}
-                                rules={{
-                                    required: t('global_error_emailRequired'),
-                                    pattern: {
-                                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                                        message: t('global_error_invalidEmail')
-                                    }
-                                }}
-                                render={({ field }) => (
-                                    <input
-                                        type="email"
-                                        {...field}
-                                        placeholder={t('global_emailPlaceholder')}
-                                        className="login-modal-input"
-                                        onInput={(e: React.ChangeEvent<HTMLInputElement>) => handleEmailValidation(e, t)}
+        <div>
+            {/* Modal de inicio de sesión */}
+            {open && (
+                <div className={`modal-overlay ${open ? 'show' : ''}`} onClick={handleOverlayClick}>
+                    <div className={`login-modal ${theme}`}>
+                        {/* Botón de cerrar */}
+                        <button className="close-button" onClick={() => setOpen(false)}>&times;</button>
+                        <div className="login-modal-header">{t('global_login')}</div>
+                        <div className="login-modal-body">
+                            <form onSubmit={handleSubmit(onSubmit)} className="login-modal-form">
+                                <div className="login-modal-field">
+                                    <label className="login-modal-label">{t('global_Email')}</label>
+                                    <Controller
+                                        name="email"
+                                        control={control}
+                                        rules={{
+                                            required: t('global_error_emailRequired'),
+                                            pattern: {
+                                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                                message: t('global_error_invalidEmail')
+                                            }
+                                        }}
+                                        render={({ field }) => (
+                                            <input
+                                                type="email"
+                                                {...field}
+                                                placeholder={t('global_emailPlaceholder')}
+                                                className="login-modal-input"
+                                                onInput={(e: React.ChangeEvent<HTMLInputElement>) => handleEmailValidation(e, t)}
+                                            />
+                                        )}
                                     />
-                                )}
-                            />
-                            {errors.email && <p className="login-modal-error">{errors.email.message}</p>}
-                        </div>
+                                    {errors.email && <p className="login-modal-error">{errors.email.message}</p>}
+                                </div>
 
-                        <div className="login-modal-field">
-                            <label className="login-modal-label">{t('global_password')}</label>
-                            <div className="password-container">
-                                <Controller
-                                    name="password"
-                                    control={control}
-                                    rules={{ required: t('global_error_passwordRequired') }}
-                                    render={({ field }) => (
-                                        <input
-                                            type={showPassword ? "text" : "password"}
-                                            {...field}
-                                            placeholder={t('login_passwordPlaceholder')}
-                                            className="login-modal-input password-input"
+                                <div className="login-modal-field">
+                                    <label className="login-modal-label">{t('global_password')}</label>
+                                    <div className="password-container">
+                                        <Controller
+                                            name="password"
+                                            control={control}
+                                            rules={{ required: t('global_error_passwordRequired') }}
+                                            render={({ field }) => (
+                                                <input
+                                                    type={showPassword ? "text" : "password"}
+                                                    {...field}
+                                                    placeholder={t('login_passwordPlaceholder')}
+                                                    className="login-modal-input password-input"
+                                                />
+                                            )}
                                         />
-                                    )}
-                                />
-                                <span className="toggle-password" onClick={togglePasswordVisibility}>
-                                    {showPassword ? <FaLockOpen /> : <FaLock />}
-                                </span>
-                            </div>
-                            {errors.password && <p className="login-modal-error">{errors.password.message}</p>}
-                        </div>
+                                        <span className="toggle-password" onClick={togglePasswordVisibility}>
+                                            {showPassword ? <FaLockOpen /> : <FaLock />}
+                                        </span>
+                                    </div>
+                                    {errors.password && <p className="login-modal-error">{errors.password.message}</p>}
+                                </div>
 
-                        <button type="submit" className="login-modal-submit">
-                            {t('global_login')}
-                        </button>
+                                <button type="submit" className="login-modal-submit">
+                                    {t('global_login')}
+                                </button>
 
-                        <div className="login-modal-footer">
-                            <p className="forgot-password-text">
-                                {t('login_forgotPassword')} <a href="/forgot-password" className="forgot-password-link">{t('login_recoverPassword')}</a>
-                            </p>
+                                <div className="login-modal-footer">
+                                    <p className="forgot-password-text">
+                                        {t('login_forgotPassword')}
+                                        &nbsp;
+                                        <a href="#" onClick={handleRecoverPasswordClick} className="forgot-password-link">
+                                            {t('login_recoverPassword')}
+                                        </a>
+                                    </p>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+            )}
+
+            {openRecoverPasswordModal && (
+                <RecoverPasswordModal
+                    open={openRecoverPasswordModal}
+                    setOpen={setOpenRecoverPasswordModal}
+                    onBackToLogin={handleBackToLogin}
+                />
+            )}
         </div>
     );
 };
