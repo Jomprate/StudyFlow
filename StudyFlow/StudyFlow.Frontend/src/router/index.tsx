9575e67391ecb638e17d5a7bf00dbaad965def6f
Davlopez06 <davidlopezvaldes@gmail.com>
2024-09-24 17:37:22 -0500

    [FEATURE] front subject

--- StudyFlow/StudyFlow.Frontend/src/router/index.tsx
+++ StudyFlow/StudyFlow.Frontend/src/router/index.tsx
@@ -7,6 +7,7 @@ import Notifications from '../pages/Notifications/Notifications.tsx';
 import Requests from '../pages/Requests/Requests.tsx';
 import Settings from '../pages/Settings/Settings.tsx';
 import Course from '../pages/Course/course.tsx';
+import Subject from '../pages/Subjects/Subject.tsx';
 
 const Router = () => {
     return (
@@ -14,6 +15,7 @@ const Router = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/countries" element={<Countries />} />
+                <Route path="/subject" element={<Subject />} />
                 <Route path="/home_logged_in" element={<LoggedInMainLayout />}>
                     <Route path="courses" element={<Courses />} />
                     <Route path="course" element={<Course />} />
